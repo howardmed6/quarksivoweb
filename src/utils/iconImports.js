b061b8581223cca4fdbba9ca8d7a3f37354afe2c
Howard Medina Pineda <kunkuski@gmail.com>
2025-08-17 19:19:48 +0000

    reghdfhdfhd

---
+++ src/utils/iconImports.js
@@ -0,0 +1,12 @@
+// utils/iconImports.js
+import reloadIcon from "../assets/icons/reload.svg";
+import optimizeIcon from "../assets/icons/optimize.svg";
+import qualityIcon from "../assets/icons/quality.svg";
+import noiseIcon from "../assets/icons/noise.svg";
+
+export {
+  reloadIcon,
+  optimizeIcon,
+  qualityIcon,
+  noiseIcon
+};
